a46abbc601f7dce2436fa7cd2ed029ecb6ab2844
Gabriel <biel.pessotti@yahoo.com.br>
2024-06-20 17:58:49 -0300

    primeiro_teste_bluetooth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'medication_schedule_screen.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,20 +9,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Caixa de Medicamentos',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(
-          color: Color.fromARGB(255, 168, 107, 179), // Cor de fundo da AppBar
-          titleTextStyle: TextStyle(
-            color: Colors.white, // Cor do texto
-            fontSize: 20, // Tamanho da fonte
-          ),
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: BluetoothApp(),
+    );
+  }
+}
+
+class BluetoothApp extends StatefulWidget {
+  @override
+  _BluetoothAppState createState() => _BluetoothAppState();
+}
+
+class _BluetoothAppState extends State<BluetoothApp> {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  BluetoothConnection? _connection;
+  bool _isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    FlutterBluetoothSerial.instance.onStateChanged().listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+  }
+
+  void _connectToDevice(String address) async {
+    try {
+      _connection = await BluetoothConnection.toAddress(address);
+      setState(() {
+        _isConnected = true;
+      });
+      print('Connected to the device');
+
+      _connection!.input!.listen((Uint8List data) {
+        print('Data incoming: ${ascii.decode(data)}');
+      }).onDone(() {
+        print('Disconnected by remote request');
+        setState(() {
+          _isConnected = false;
+        });
+      });
+    } catch (e) {
+      print('Cannot connect, exception occurred: $e');
+    }
+  }
+
+  void _disconnectFromDevice() async {
+    await _connection?.close();
+    setState(() {
+      _isConnected = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Bluetooth Serial Example'),
       ),
-      home: MedicationScheduleScreen(),
-      debugShowCheckedModeBanner: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Bluetooth State: $_bluetoothState'),
+            SizedBox(height: 20),
+            _isConnected
+                ? ElevatedButton(
+                    onPressed: _disconnectFromDevice,
+                    child: Text('Disconnect'),
+                  )
+                : ElevatedButton(
+                    onPressed: () => _connectToDevice('XX:XX:XX:XX:XX:XX'), // Substitua pelo endereÃ§o do seu dispositivo
+                    child: Text('Connect'),
+                  ),
+          ],
+        ),
+      ),
     );
   }
 }
